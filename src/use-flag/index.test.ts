4b9544aabd37a54453042f120517f8868211fac5
Furrya Black <furrya.black@gmail.com>
2022-01-11 17:21:08 +0300

    test: rewrite tests with vtu tools

--- src/use-flag/index.test.ts
+++ src/use-flag/index.test.ts
@@ -1,51 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
 import { useFlag } from "./index";
 
 describe("useFlag", () => {
-  it("Creation without signature will equal to false", () => {
-    const [flag] = useFlag();
-    expect(flag.value).toEqual(false);
-  });
+  it("Boolean flags have expected values", () => {
+    const wrapper = shallowMount({
+      template: `<div id="app" />`,
 
-  it("Creation with truthly signature will equal to true", () => {
-    const fetchedItems = [{id: 1}];
-    const [flag] = useFlag(Boolean(fetchedItems.length));
-    expect(flag.value).toEqual(true);
-  });
+      setup() {
+        const [flagLeft] = useFlag();
+        const [flagRight] = useFlag(true);
+        return {flagLeft, flagRight}
+      },
+    });
 
-  it("flagApi.set works as expected", () => {
-    const [hasItems, {set: setHasItems}] = useFlag();
-    const fetchedItems = [{id: 1}];
-    setHasItems(Boolean(fetchedItems.length));
-    expect(hasItems.value).toEqual(true);
-  });
-
-  it("flagApi.toggle works as expected", () => {
-    const [flag, {toggle}] = useFlag();
-    expect(flag.value).toEqual(false);
-    toggle();
-    expect(flag.value).toEqual(true);
-  });
-
-  it("flagApi.setTrue works as expected", () => {
-    const [flag, {setTrue}] = useFlag(false);
-    expect(flag.value).toEqual(false);
-    setTrue();
-    expect(flag.value).toEqual(true);
-  });
-
-  it("flagApi.setFalse works as expected", () => {
-    const [flag, {setFalse}] = useFlag(true);
-    expect(flag.value).toEqual(true);
-    setFalse();
-    expect(flag.value).toEqual(false);
-  });
-
-  it("flagApi.reset works as expected", () => {
-    const [flag, {toggle, reset}] = useFlag();
-    expect(flag.value).toEqual(false);
-    toggle();
-    expect(flag.value).toEqual(true);
-    reset();
-    expect(flag.value).toEqual(false);
+    expect(wrapper.vm.flagLeft).toEqual(false);
+    expect(wrapper.vm.flagRight).toEqual(true);
   });
 });
--- src/use-state/index.test.ts
+++ src/use-state/index.test.ts
@@ -1,53 +1,19 @@
+import { shallowMount } from "@vue/test-utils";
 import { useState } from "./index";
 
 describe("useState", () => {
-  it("Creation with signature 3 will equal to 3", () => {
-    const [acc] = useState<number>(3);
-    expect(acc.value).toEqual(3);
-  });
+  it("User state has expected value", () => {
+    const userShame = {id: 123, name: "Fu Shame"};
 
-  it("stateApi.dispatch works as expected", () => {
-    const [acc, { dispatch }] = useState<{ name: string }>({ name: "Furrya" });
-    dispatch({ name: "Black" });
-    expect(acc.value).toEqual({ name: "Black" });
-  });
+    const wrapper = shallowMount({
+      template: `<div id="app" />`,
 
-  it("stateApi.trigger works as expected", () => {
-    const mk = (id: number) => ({id});
+      setup() {
+        const [user] = useState(userShame);
+        return {user};
+      },
+    });
 
-    const [
-      acc,
-      { trigger: inc },
-    ] = useState<Array<ReturnType<typeof mk>> | null>(null, (current) => ([...(current ?? []), mk((current?.length || 0) + 1)]));
-
-    inc();
-    inc();
-    inc();
-    inc();
-
-    expect(acc.value).toEqual([
-      {id: 1},
-      {id: 2},
-      {id: 3},
-      {id: 4},
-    ]);
-  });
-
-  it("stateApi.reset works as expected", () => {
-    const [acc, { dispatch, reset }] = useState<number>(1, (current) => current + 1);
-    dispatch(100);
-    expect(acc.value).toEqual(2);
-    dispatch(50);
-    expect(acc.value).toEqual(3);
-    reset();
-    expect(acc.value).toEqual(1);
-  });
-
-  it("mapFn: ignore next value with {dispatch}", () => {
-    const [acc, { dispatch }] = useState<number>(1, (current) => current + 1);
-    dispatch(100);
-    expect(acc.value).toEqual(2);
-    dispatch(50);
-    expect(acc.value).toEqual(3);
+    expect(wrapper.vm.user).toEqual(userShame);
   });
 });
